179c2883ab72db28e367a9be9cbc476c0cbe89a4
leon <leonyan91@163.com>
2017-07-23 18:20:00 +0800

    功能添加

--- src/app/layout/form/form.module.ts
+++ src/app/layout/form/form.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { FormRoutingModule } from './form-routing.module';
 import { FormComponent } from './form.component';
 import { PageHeaderModule } from './../../shared';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
     imports: [
         CommonModule,
         FormRoutingModule,
-        PageHeaderModule
+        PageHeaderModule,
+        HttpClientModule,
+        FormsModule
     ],
     declarations: [FormComponent]
 })
