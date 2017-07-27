5da40162d19ea536ba8c0be172fff39108aab0e2
leonyan91 <leonyan91@163.com>
2017-07-27 18:55:58 +0800

    20170727 功能开发

--- src/app/layout/user/user-record.component.ts
+++ src/app/layout/user/user-record.component.ts
@@ -1,6 +1,11 @@
+import {Component} from "@angular/core";
 /**
  * Created by mac on 2017/7/27.
  */
+
+@Component({
+    templateUrl: 'user-record.component.html'
+})
 export class UserRecordComponent{
 
 }
