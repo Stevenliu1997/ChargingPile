35061519dd41fdfaadbfa0da59ef79cd33328e5e
leon <leonyan91@163.com>
2017-08-06 18:16:42 +0800

    表达验证

--- src/app/layout/user/user-edit.component.ts
+++ src/app/layout/user/user-edit.component.ts
@@ -1,9 +1,10 @@
 /**
  * Created by mac on 2017/7/27.
  */
-import {Component, Input} from '@angular/core';
+import {Component, Input, ViewChild} from '@angular/core';
 import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
 import {CustomHttpClient} from "../../shared/services/custom-http-client/CustomHttpClient";
+import {NgForm} from "@angular/forms";
 
 @Component({
     selector: 'user-edit',
@@ -16,9 +17,16 @@ export class UserEditComponent {
     @Input()
     editModel: any = {};
 
+    @ViewChild('submitForm')
+    editForm: NgForm;
+
     constructor(public activeModal: NgbActiveModal,private customHttpClient: CustomHttpClient) {}
 
     confirm() {
+
+        if(this.editForm.form.invalid){
+            return;
+        }
         if(this.actionTitle === '添加'){
             this.addUser(this.editModel);
         }else {
