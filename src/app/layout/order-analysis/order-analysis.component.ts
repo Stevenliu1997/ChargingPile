856f61abf75e8d95056adda4c4cc048e3364e093
WeiWei <838286494@qq.com>
2017-08-15 11:15:50 +0800

    weiwei

--- src/app/layout/order-analysis/order-analysis.component.ts
+++ src/app/layout/order-analysis/order-analysis.component.ts
@@ -1,8 +1,9 @@
 import {Component, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
 import {routerTransition} from '../../router.animations';
 import {DatagridComponent} from '../../shared/components/widget/datagrid/datagrid.component';
-import {NgbModal, NgbTabChangeEvent} from '@ng-bootstrap/ng-bootstrap';
+import {NgbTabChangeEvent} from '@ng-bootstrap/ng-bootstrap';
 import {ToastsManager} from 'ng2-toastr';
+import {CustomHttpClient} from '../../shared/services/custom-http-client/CustomHttpClient';
 
 @Component({
     selector: 'app-order-analysis',
@@ -19,14 +20,58 @@ export class OrderAnalysisComponent implements OnInit {
     private moneyCountComponent: DatagridComponent;
 
     queryModel: any = {};
+    /*图标查询数据*/
+    chartObj: any = {
+        findType: '1',
+        timeType: '1',
+        starttime: '',
+        endtime: ''
+    };
+    /*存储曲线数组*/
+    lineArray: any = [{data: [], label: ''}];
+    /*横坐标*/
+    lineLabelArray: Array<any> = [];
+    /*options*/
+    public lineChartOptions: any = {
+        responsive: true
+    };
+    /*colors*/
+    public lineChartColors: Array<any> = [
+        { // grey
+            backgroundColor: 'rgba(148,159,177,0.2)',
+            borderColor: 'rgba(148,159,177,1)',
+            pointBackgroundColor: 'rgba(148,159,177,1)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+        },
+        { // dark grey
+            backgroundColor: 'rgba(77,83,96,0.2)',
+            borderColor: 'rgba(77,83,96,1)',
+            pointBackgroundColor: 'rgba(77,83,96,1)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgba(77,83,96,1)'
+        },
+        { // grey
+            backgroundColor: 'rgba(148,159,177,0.2)',
+            borderColor: 'rgba(148,159,177,1)',
+            pointBackgroundColor: 'rgba(148,159,177,1)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+        }
+    ];
+    public lineChartLegend: boolean = true;
+    public lineChartType: string = 'line';
 
     orderCConfig: object = {
         key: 'orderid',
-        url: 'OrderAnalysis/Find',
+        url: 'orderform/analyse/form',
         column: [
-            {name: '日期', key: 'orderdate'},
-            {name: '名称', key: 'ordername'},
-            {name: '订单数量', key: 'ordernumber'}
+            {name: '日期', key: 'time'},
+            {name: '名称', key: 'objectname'},
+            {name: '订单数量', key: 'analyseData'}
         ],
         params: function () {
             return this.queryModel;
@@ -60,7 +105,7 @@ export class OrderAnalysisComponent implements OnInit {
         }.bind(this),
     };
     constructor(
-        private ngbModal: NgbModal,
+        private customHttpClient: CustomHttpClient,
         public toastr: ToastsManager,
         vcr: ViewContainerRef
     ) {
@@ -70,17 +115,15 @@ export class OrderAnalysisComponent implements OnInit {
 
     }
     ngOnInit() {
+        this.find();
     }
     beforeChange($event: NgbTabChangeEvent) {
-        if ($event.activeId === 'siteManagement') {
-            this.chargingclear();
-            this.moneyclear();
-        } else if ($event.activeId === 'chargingRule') {
-            this.orderclear();
-            this.moneyclear();
-        } else if ($event.activeId === 'articleManagement') {
-            this.orderclear();
-            this.chargingclear();
+        if ($event.activeId === 'orderstatistics') {
+            this.chartObj.findType = '1';
+        } else if ($event.activeId === 'chargestatistics') {
+            this.chartObj.findType = '2';
+        } else if ($event.activeId === 'moneystatistics') {
+            this.chartObj.findType = '3';
         }
     }
     refreshGridOrderC() {
@@ -99,62 +142,37 @@ export class OrderAnalysisComponent implements OnInit {
     }
     moneyclear(): void {
     }
-    // lineChart
-    public lineChartData:Array<any> = [
-        {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-        {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-        {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
-    ];
-    public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-    public lineChartOptions:any = {
-        responsive: true
-    };
-    public lineChartColors:Array<any> = [
-        { // grey
-            backgroundColor: 'rgba(148,159,177,0.2)',
-            borderColor: 'rgba(148,159,177,1)',
-            pointBackgroundColor: 'rgba(148,159,177,1)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-        },
-        { // dark grey
-            backgroundColor: 'rgba(77,83,96,0.2)',
-            borderColor: 'rgba(77,83,96,1)',
-            pointBackgroundColor: 'rgba(77,83,96,1)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgba(77,83,96,1)'
-        },
-        { // grey
-            backgroundColor: 'rgba(148,159,177,0.2)',
-            borderColor: 'rgba(148,159,177,1)',
-            pointBackgroundColor: 'rgba(148,159,177,1)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-        }
-    ];
-    public lineChartLegend:boolean = true;
-    public lineChartType:string = 'line';
 
-    public randomize():void {
-        let _lineChartData:Array<any> = new Array(this.lineChartData.length);
-        for (let i = 0; i < this.lineChartData.length; i++) {
-            _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
-            for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-                _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
-            }
-        }
-        this.lineChartData = _lineChartData;
+    year() {
+        this.chartObj.timeType = '1';
+        this.find();
     }
-
-    // events
-    public chartClicked(e:any):void {
+    month() {
+        this.chartObj.timeType = '2';
+        this.find();
+    }
+    day() {
+        this.chartObj.timeType = '3';
+        this.find();
+    }
+    find() {
+        this.customHttpClient.post('orderform/analyse/group', this.chartObj).subscribe(result => {
+            if (result.code === '00') {
+                for (let i = 0; i < result.data.length; i++) {
+                    let tempObj: any = {};
+                    tempObj.data = result.data[i].analyseData;
+                    tempObj.label = result.data[i].objectname;
+                    this.lineArray[i] = tempObj;
+                }
+                this.lineLabelArray = result.time;
+            }
+        });
+    }
+    public chartClicked(e: any): void {
         console.log(e);
     }
 
-    public chartHovered(e:any):void {
+    public chartHovered(e: any): void {
         console.log(e);
     }
 }
