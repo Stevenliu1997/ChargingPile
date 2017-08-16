cb600b34eab4589d8d8de262810651a1f45a92a9
leonyan91 <leonyan91@163.com>
2017-08-16 11:12:01 +0800

    开发订单分析

--- src/app/layout/order-analysis/order-analysis.component.ts
+++ src/app/layout/order-analysis/order-analysis.component.ts
@@ -21,6 +21,8 @@ export class OrderAnalysisComponent implements OnInit {
     @ViewChild('moneyCount')
     private moneyCountComponent: DatagridComponent;
 
+    private formUrl: string = 'OrderForm/analysis/form';
+    private chartUrl: string = 'OrderForm/analysis/group';
     //tabs id
     tabsConfig: any;
 
@@ -30,6 +32,8 @@ export class OrderAnalysisComponent implements OnInit {
     queryModel: any = {};
     //datagrid配置
     datagridConfigs: any = {};
+    //图表数据
+    chartConfig: any = {};
 
     constructor(private customHttpClient: CustomHttpClient){}
 
@@ -86,6 +90,7 @@ export class OrderAnalysisComponent implements OnInit {
         //初始化查询对象
         this.initQueryModel();
         this.initDatagridConfig();
+        this.initChartConfig();
     }
 
     /**
@@ -106,9 +111,16 @@ export class OrderAnalysisComponent implements OnInit {
      */
     initQueryModel(){
         for(let i in this.tabsConfig){
-            //默认为选择日
-            this.queryModel[this.tabsConfig[i].key] = '1';
+            //默认为选择年
+            this.initQueryModelSingle(this.tabsConfig[i].key);
         }
+    }
+
+    initQueryModelSingle(key: string){
+        this.queryModel[key] = {
+            timeType: 1,
+            checkedSite: []
+        };
     }
 
     //初始化datagrid
@@ -116,13 +128,26 @@ export class OrderAnalysisComponent implements OnInit {
         for(let i in this.tabsConfig){
             let tabConfig = this.tabsConfig[i];
             let config = {
-                url: 'orderForm/analysis/form',//现在三个tab都用一个url
+                url: this.formUrl,//现在三个tab都用一个url
                 column: tabConfig.gridConfig.column,
                 params: () => {
+                    //TODO 站点选择
                     return Object.assign({}, this.queryModel[tabConfig.key], {findType: tabConfig.findType})
                 },
             };
             this.datagridConfigs[tabConfig.key] = config;
+        }
+    }
+
+    /**
+     * 初始化图表基本数据
+     */
+    initChartConfig(){
+        for(let i in this.tabsConfig){
+            let tabConfig = this.tabsConfig[i];
+            this.chartConfig[tabConfig.key] = {
+                options: {responsive: true}
+            };
         }
     }
 
@@ -132,10 +157,56 @@ export class OrderAnalysisComponent implements OnInit {
      */
     refreshGrid(key: string){
         this[`${key}Component`].refreshGrid();
+        //点击查询，也要重新刷新图表
+        this.refreshChart(key)
+    }
+
+    /**
+     * 清空查询条件
+     * @param key
+     */
+    clearQueryModel(key: string){
+        this.initQueryModelSingle(key);
+    }
+
+    changeTimeType(key: string, type: number){
+        this.queryModel[key].timeType = type;
+        this.refreshChart(key);
     }
 
     refreshChart(key: string){
-        this.customHttpClient.post('')
+        this.customHttpClient.post(this.chartUrl, this.queryModel[key]).subscribe(result => {
+            if(result.code === '00'){
+                this.buildChartData(result.data, key);
+            }
+        });
+    }
+
+    buildChartData(data: any, key: string){
+        let keyObj = this.chartConfig[key];
+        keyObj.datasets = [];
+        keyObj.colors = [];
+        for(var i in data){
+            let currentItem = data[i];
+            //收支统计返回的analyseData数组中有又是数据，因为有三种数据
+            let dataTemp = currentItem.analyseData[0] instanceof Array ?
+                currentItem.analyseData.map(item => {
+                    return item[0];
+                }) : currentItem.analyseData;
+            keyObj.datasets.push({data: dataTemp, label: currentItem.sitename});
+            keyObj.labels = currentItem.time;
+            //TODO 随机一个站点生成颜色，现在全是一种
+            keyObj.colors.push({ // grey
+                backgroundColor: 'rgba(148,159,177,0.2)',
+                borderColor: 'rgba(148,159,177,1)',
+                pointBackgroundColor: 'rgba(148,159,177,1)',
+                pointBorderColor: '#fff',
+                pointHoverBackgroundColor: '#fff',
+                pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+            });
+
+
+        }
     }
 
     beforeChange(event: any){
