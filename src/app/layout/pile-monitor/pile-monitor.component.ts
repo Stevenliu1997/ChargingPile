f1ff7f38d67b6d82acb588ca612a6b2f853c3c17
WeiWei <838286494@qq.com>
2017-08-20 18:14:00 +0800

    weiwei

--- src/app/layout/pile-monitor/pile-monitor.component.ts
+++ src/app/layout/pile-monitor/pile-monitor.component.ts
@@ -23,45 +23,45 @@ export class PileMonitorComponent implements OnInit {
     };
 
     config: any = {
-        key: 'brandid',
-        url: 'PileMonitor/Find',
+        key: '',
+        url: 'Site_Pile_Gun/Find',
         column: [
             {name: '站点名', key: 'sitename'},
             {name: '厂家', key: 'factory', html: function(item) {
                 return `<a href="www.baidu.com">查看</a>`;
             }},
-            {name: '型号', key: 'model'},
-            {name: '名称', key: 'name'},
-            {name: '安装时间', key: 'settime'},
+            {name: '型号', key: 'piletype'},
+            {name: '名称', key: 'pilename'},
+            {name: '安装时间', key: 'installationtime'},
             {name: '枪头ID', key: 'gunid'},
-            {name: '类型', key: 'type'},
+            {name: '类型', key: 'guntype'},
             {name: '告警', key: 'alarm', html: function(item) {
                 return `<a href="www.baidu.com">查看</a>`;
             }},
             {name: '交易记录', key: 'record', html: function(item) {
                 return `<a href="www.baidu.com">查看</a>`;
             }},
-            {name: '预约状态', key: 'orderstatus'},
+            {name: '预约状态', key: 'reservestate'},
             {name: '是否在线', key: 'isonline', html: function (item) {
                 if (item.isonline === true) {
-                    return `<i class="fa fa-link" aria-hidden="true" (click)="this.test()"></i>`;
+                    return `<i class="fa fa-link" aria-hidden="true" (click)="function(){alert('w');}"></i>`;
                 } else if (item.isonline === false) {
-                    return `<i class="fa fa-chain-broken" aria-hidden="true" (click)="this.test()")></i>`;
+                    return `<i class="fa fa-chain-broken" aria-hidden="true" (click)="function(){alert('w');}")></i>`;
                 } else {
-                    return `<i class="fa fa-chain-broken" aria-hidden="true" (click)="this.test()"></i>`;
+                    return `<i class="fa fa-chain-broken" aria-hidden="true" (click)="function(){alert('w');}"></i>`;
                 }
             }},
             {name: '输出电压', key: 'v'},
             {name: '输出电流', key: 'i'},
             {name: '输出功率', key: 'p'},
-            {name: '累计电量', key: 'power'},
-            {name: '状态', key: 'status', html: function(item) {
+            {name: '累计电量', key: 'gunchargeamount'},
+            {name: '状态', key: 'gunstate', html: function(item) {
                 return `<i class="fa mr-2" [ngClass]="getIcon(rowAct, data)" aria-hidden="true" (click)="rowAction(rowAct, data)"></i>`
             }},
             {name: '有无', key: 'has'},
             {name: '编号', key: 'number'},
-            {name: '状态', key: 'statusa'},
-            {name: '停车状态', key: 'car'}
+            {name: '状态', key: 'state'},
+            {name: '停车状态', key: 'parking'}
         ],
         pageSize: 20,
         pageSizes: [20, 50, 100],
@@ -73,11 +73,8 @@ export class PileMonitorComponent implements OnInit {
     constructor(
         private httpClient: CustomHttpClient,
     ) {}
-
-    test() {
-        alert('ww');
-    }
     ngOnInit() {
+        this.clear();
         this.pageParams = {
             pageNumber: 1,
             pageSize: this.config.pageSize
@@ -92,7 +89,10 @@ export class PileMonitorComponent implements OnInit {
         this.loadData(this.config.url, this.config.params());
     }
     clear(): void {
-        this.queryModel = {};
+        this.queryModel.province = '';
+        this.queryModel.city = '';
+        this.queryModel.district = '';
+        this.queryModel.sitename = '';
     }
     public loadData(ur: string, params?: object, pageParams?: any): void{
         this.httpClient.post(this.config.url, Object.assign({}, params, Object.assign({}, this.pageParams, pageParams)))
